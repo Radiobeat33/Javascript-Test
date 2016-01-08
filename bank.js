5670c2bdb32c34c03e92474ef4fae88d8a5a5a8d
Steven Williams <Steven30135@gmail.com>
2016-01-08 00:55:03 -0800

    First commit, did all major refactoring. Just adding comments and final tweaks

--- bank.js
+++ bank.js
@@ -1,67 +1,63 @@
-var balance1 = 0;
-var balance2 = 0;
+(function() {
 
-$(function(){
+    window.onload = function() {
 
-  $('#deposit1').click(depositAccount1);
-  $('#deposit2').click(depositAccount2);
-  $('#withdraw1').click(withdrawAccount1);
-  $('#withdraw2').click(withdrawAccount2);
-  updateDisplay();
-});
+        var checkingBalance = 0;
+        var retirementBalance = 0;
 
-function depositAccount1()
-{
-  var amount = $('#amount1').val();
-  amount = parseInt(amount);
-  balance1 = amount + balance1;
-  updateDisplay();
-}
+        // Method that takes in an array of class selectors and callback functions, this function runs once the page has loaded (see below)
+        var SetClickHandlers = {
+            assign: function(arr) {
+                arr.forEach(function(value, index) {
+                    document.getElementById(arr[index][0]).addEventListener("click", function(){ arr[index][2](arr[index][1], arr[index][0]) });
+                    Account.updateDisplay(arr[index][1]);
+                });
+            }
+        };
 
-function depositAccount2()
-{
-  var amount = $('#amount2').val();
-  amount = parseInt(amount);
-  amount = amount + 10;
-  balance2 = amount + balance2;
-  updateDisplay();
-}
+        var Account = {
+            deposit: function(amountElementId) {
+                var amount = parseInt(document.getElementById(amountElementId).value);
+                amountElementId === 'amount1' ? checkingBalance += amount : retirementBalance += amount + 10;
+                Account.updateDisplay(amountElementId);
+            },
+            withdraw: function(amountElementId) {
+                var amount = parseInt(document.getElementById(amountElementId).value);
+                if (amountElementId === 'amount1' && amount <= checkingBalance) {
+                    checkingBalance -= amount;
+                } else if (amountElementId === 'amount2' && amount <= retirementBalance) {
+                    retirementBalance -= amount;
+                }
+                Account.updateDisplay(amountElementId);
+            },
+            updateDisplay: function(amountElementId){
+                var currentBalance;
+                var currentBalanceElementId;
+                if(amountElementId === 'amount1'){
+                    currentBalance = checkingBalance;
+                    currentBalanceElementId = 'checkingBalance';
+                } else {
+                    currentBalance = retirementBalance;
+                    currentBalanceElementId = 'retirementBalance';
+                }
+                currentBalance <= 0 ? document.getElementById(currentBalanceElementId).classList.add("zero") : document.getElementById(currentBalanceElementId).classList.remove("zero");
+                document.getElementById(currentBalanceElementId).innerHTML = '$' + currentBalance;
+                document.getElementById(amountElementId).value = '';
+            }
+        };
+        // Storage for all class selectors and their corresponding callback function
+        var classAndCallbackStorage = [
+            ["deposit1", "amount1", Account.deposit],
+            ["deposit2", "amount2", Account.deposit],
+            ["withdraw1", "amount1", Account.withdraw],
+            ["withdraw2", "amount2", Account.withdraw]
+        ];
 
-function withdrawAccount1()
-{
-  var amount = $('#amount1').val();
-  amount = parseInt(amount);
-  if(amount <= balance1) {
-    balance1 = balance1 - amount;
-  }
-  updateDisplay();
-}
+        // invoking the assign method within setClickHandlers with the classAndCallbackStorage array passed in as an argument
+        SetClickHandlers.assign(classAndCallbackStorage);
 
-function withdrawAccount2()
-{
-  var amount = $('#amount2').val();
-  amount = parseInt(amount);
-  if(amount <= balance2) {
-    balance2 = balance2 - amount;
-  }
-  updateDisplay();
-}
+    }
+}());
 
-function updateDisplay()
-{
-  if(balance1 <= 0)
-    $('#balance1').addClass('zero');
-  else
-    $('#balance1').removeClass('zero');
 
-  if(balance2 <= 0)
-    $('#balance2').addClass('zero');
-  else
-    $('#balance2').removeClass('zero');
-
-  $('#balance1').text('$' + balance1);
-  $('#balance2').text('$' + balance2);
-  $('#amount1').val('');
-  $('#amount2').val('');
-}
 
